59c44552a142803b852eb3d1131d2c7d5945c05d
Slim Pear <slimpear@gmail.com>
2019-01-20 17:51:56 +0100

    4

--- js/11.3.js
+++ js/11.3.js
@@ -25,6 +25,64 @@ const restaurantsList = [
     },
 ];
 
+let b = '{'+
+    '"restaurantsList": {'+
+        '"res1": {'+
+            '"name":     "LuLa am Markt",'+
+            '"subtitle": "Café",'+
+            '"img":      "https://lh5.googleusercontent.com/p/AF1QipMHUGKUTwadfNz-tuMr4OSbuH4ng5IKrqQoLk7w=w284-h160-k-no",'+
+            '"address":  "Lauterstraße 14, 12159 Berlin",'+
+            '"url":      "http://lula-berlin.de",'+
+            '"tel":      "030 12345678"'+
+        '},'+
+        '"res2": {'+
+            '"name":     "KAWA Cafe-Deli",'+
+            '"subtitle": "Veganes Restaurant",'+
+            '"img":      "https://lh5.googleusercontent.com/p/AF1QipOfkiCHPK6KAIMyrGtm-184QCauoIwmO6sTiRPK=w213-h160-k-no",'+
+            '"address":  "Moltkestraße 1, 12203 Berlin",'+
+            '"url":      "http://www.cafekawa.de",'+
+            '"tel":      "030 12345678"'+
+        '},'+
+        '"res3": {'+
+            '"name":     "Curry 36",'+
+            '"subtitle": "Snack Bar",'+
+            '"img":      "https://lh5.googleusercontent.com/p/AF1QipOTqap7-04d-rfhnlI8EgOvXWX_Z73ZE2B3ts2B=w213-h160-k-no",'+
+            '"address":  "Mehringdamm 36, 10961 Berlin",'+
+            '"url":      "http://www.curry36.de",'+
+            '"tel":      "030 12345678"'+
+        '}'+
+    '}'+
+'}'
+
+let a = '{'+
+    '"restaurantsList": '+
+    '['+
+        '{'+
+            '"name":     "LuLa am Markt",'+
+            '"subtitle": "Café",'+
+            '"img":      "https://lh5.googleusercontent.com/p/AF1QipMHUGKUTwadfNz-tuMr4OSbuH4ng5IKrqQoLk7w=w284-h160-k-no",'+
+            '"address":  "Lauterstraße 14, 12159 Berlin",'+
+            '"url":      "http://lula-berlin.de",'+
+            '"tel":      "030 12345678"'+
+        '},'+
+        '{'+
+            '"name":     "KAWA Cafe-Deli",'+
+            '"subtitle": "Veganes Restaurant",'+
+            '"img":      "https://lh5.googleusercontent.com/p/AF1QipOfkiCHPK6KAIMyrGtm-184QCauoIwmO6sTiRPK=w213-h160-k-no",'+
+            '"address":  "Moltkestraße 1, 12203 Berlin",'+
+            '"url":      "http://www.cafekawa.de",'+
+            '"tel":      "030 12345678"'+
+        '},'+
+        '{'+
+            '"name":     "Curry 36",'+
+            '"subtitle": "Snack Bar",'+
+            '"img":      "https://lh5.googleusercontent.com/p/AF1QipOTqap7-04d-rfhnlI8EgOvXWX_Z73ZE2B3ts2B=w213-h160-k-no",'+
+            '"address":  "Mehringdamm 36, 10961 Berlin",'+
+            '"url":      "http://www.curry36.de",'+
+            '"tel":      "030 12345678"'+
+        '}'+
+    ']'+
+'}'
 
 hashchangeblabla();
 
@@ -90,18 +148,23 @@ function jumpRestaurants() {
     } else { //9.2.4
         $("main").empty();
         let data = getRestaurants();
+        // let data = JSON.parse(a);
 
         if (data !== null) {
 
-        
             let resText = document.createElement('h2');
             resText.innerHTML += "Restaurants";
             document.getElementsByTagName("MAIN")[0].appendChild(resText);
             let cards = document.createElement('div');
             cards.setAttribute("class", "cards");
-            
+            // console.log(data.restaurantsList.res1);
+            // console.log(data);
+
             // create and add elements on the fly before adding to "main". Could be done by adding to "main" and getElementById too.
-            data.forEach(res => {
+            // data.forEach(res => {
+            for (let i=0; i<3; i++) {
+                let res = data.restaurantsList[i];
+                console.log(res);
                 let card = document.importNode(document.querySelector("#cardTemplate").content, true);
                 
                 card.querySelector('img').src = res.img;
@@ -133,7 +196,9 @@ function jumpRestaurants() {
                 
                 // card.appendChild(section);
                 cards.appendChild(card);
-            });
+            // });
+            // break;
+            }
             var clone = document.importNode(cards, true);
             document.getElementsByTagName("MAIN")[0].appendChild(clone);
             //location.hash = "restaurants";
